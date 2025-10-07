b54a07dde0f9ad3dab97c83712736cd2a451a42d
Shivendra Bhaginath Devadhe <devadheshivendra54@gmail.com>
2025-10-07 15:20:31 +0530

    updated next config for base url

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/api` : 'http://localhost:3000/api',
+  },
 };
 
 export default nextConfig;
